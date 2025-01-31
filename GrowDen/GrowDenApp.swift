9cc9753e90dd45d7a01ff9f3264305e95edc3e35
Glen112 <escobar.glen@gmail.com>
2025-01-31 08:23:16 -0300

    Initial Commit

---
+++ GrowDen/GrowDenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowDenApp.swift
+//  GrowDen
+//
+//  Created by Glen on 31-01-25.
+//
+
+import SwiftUI
+
+@main
+struct GrowDenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
